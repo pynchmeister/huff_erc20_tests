6e8e4d9d9dbdfcf9faace69d9f6a096550dc2c0c
Harris <acyclicgraphs@gmail.com>
2022-07-19 21:53:42 -0400

    Update ERC721.t.sol
    HuffDeployer seems to be working

--- src/test/ERC721.t.sol
+++ src/test/ERC721.t.sol
@@ -1,20 +1,26 @@
 pragma solidity ^0.8.15;
 
 // import "foundry-huff/HuffDeployer.sol";
-import "foundry-huff/HuffDeployer.sol";
+// import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 import "src/interfaces/IERC721.sol";
 
+import {HuffDeployer} from "src/lib/utils/HuffDeployer.sol";
+
+
 contract ERC721 is Test {
+
+    ///@notice create a new instance of HuffDeployer
+    HuffDeployer huffDeployer = new HuffDeployer();
 
     IERC721 erc721;
 
     function setUp() public {
         // Deploy a new instance of src/ERC20.huff
-        address impl = HuffDeployer.deploy("huff_contracts/ERC721");
-        erc721 = IERC721(impl);
+        erc721 = IERC721(huffDeployer.deployContract("ERC721"));
+        // erc721 = IERC721(impl);
 
     }
 
